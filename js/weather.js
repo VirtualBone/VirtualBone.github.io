e6ef0bd250cfa0016d1c902d94d8a06c03d9252f
用户名 <gameconsolecn@protonmail.com>
2022-05-09 11:35:35 +0800

    Site updated: 2022-05-09 11:35:32

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "0123",
+      "background": "5",
+      "tmpColor": "FCAF3A",
+      "tmpSize": "16",
+      "cityColor": "F2D8B1",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "borderRadius": "5",
+      "fixed": "true",
+      "vertical": "top",
+      "horizontal": "left",
+      "left": "90",
+      "top": "10",
+      "key": "398ef692ca81472f814c0af3c05efbed"
+    }
+  }
